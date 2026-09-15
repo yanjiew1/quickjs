270c3fd2c667ca2c9e10c0ea39f527287126b947
Yan-Jie Wang <yanjiewtw@gmail.com>
2026-09-15 16:49:23 +0800

    refactor: extract QuickJS allocator subsystem
    Signed-off-by: Yan-Jie Wang <yanjiewtw@gmail.com>

---
+++ src/quickjs/internal-allocator.h
@@ -0,0 +1,33 @@
+/*
+ * QuickJS Javascript Engine
+ *
+ * Copyright (c) 2017-2025 Fabrice Bellard
+ * Copyright (c) 2017-2025 Charlie Gordon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef QUICKJS_INTERNAL_ALLOCATOR_H
+#define QUICKJS_INTERNAL_ALLOCATOR_H
+
+#include "internal-types.h"
+
+QJS_INTERNAL void qjs_allocator_init(JSMallocContext *ctx);
+QJS_INTERNAL const JSMallocFunctions *qjs_default_malloc_functions(void);
+
+#endif /* QUICKJS_INTERNAL_ALLOCATOR_H */
