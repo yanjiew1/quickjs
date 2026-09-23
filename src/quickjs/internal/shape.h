9764cf7939d99236e1329341f64ac113842fc294
Yan-Jie Wang <yanjiewtw@gmail.com>
2026-09-23 22:31:38 +0800

    Extract shape hash and lifecycle module

--- src/quickjs/internal/shape.h
+++ src/quickjs/internal/shape.h
@@ -112,4 +112,49 @@ static force_inline JSShapeProperty *find_own_property1(JSObject *p,
 }
 
 
+static inline size_t get_shape_size(size_t hash_size, size_t prop_size)
+{
+    return sizeof(JSShape) + hash_size * sizeof(uint32_t) +
+        prop_size * sizeof(JSShapeProperty);
+}
+
+
+static inline JSShape *js_new_shape_nohash(JSContext *ctx, JSObject *proto,
+                                           int hash_size, int prop_size)
+{
+    JSRuntime *rt = ctx->rt;
+    JSShape *sh;
+
+    sh = js_malloc(ctx, get_shape_size(hash_size, prop_size));
+    if (!sh)
+        return NULL;
+    js_rc(sh)->ref_count = 1;
+    add_gc_object(rt, &sh->header, JS_GC_OBJ_TYPE_SHAPE);
+    if (proto)
+        JS_DupValue(ctx, JS_MKPTR(JS_TAG_OBJECT, proto));
+    sh->proto = proto;
+    memset(sh->hash_table, 0, sizeof(sh->hash_table[0]) * hash_size);
+    sh->prop_hash_mask = hash_size - 1;
+    sh->prop_size = prop_size;
+    sh->prop_count = 0;
+    sh->deleted_prop_count = 0;
+    sh->is_hashed = FALSE;
+    return sh;
+}
+
+QJS_INTERNAL JSShape *find_hashed_shape_proto(JSRuntime *rt, JSObject *proto);
+QJS_INTERNAL JSShape *js_new_shape(JSContext *ctx, JSObject *proto);
+QJS_INTERNAL JSShape *find_hashed_shape_prop(JSRuntime *rt, JSShape *sh,
+                                       JSAtom atom, int prop_flags);
+QJS_INTERNAL JSShape *js_clone_shape(JSContext *ctx, JSShape *sh1);
+QJS_INTERNAL void js_shape_hash_link(JSRuntime *rt, JSShape *sh);
+QJS_INTERNAL no_inline int resize_properties(JSContext *ctx, JSShape **psh,
+                                       JSObject *p, uint32_t count);
+QJS_INTERNAL int compact_properties(JSContext *ctx, JSObject *p);
+QJS_INTERNAL void js_shape_hash_unlink(JSRuntime *rt, JSShape *sh);
+QJS_INTERNAL void js_free_shape(JSRuntime *rt, JSShape *sh);
+QJS_INTERNAL void js_free_shape_null(JSRuntime *rt, JSShape *sh);
+QJS_INTERNAL int init_shape_hash(JSRuntime *rt);
+QJS_INTERNAL __maybe_unused void JS_DumpShapes(JSRuntime *rt);
+
 #endif /* QJS_SHAPE_H */
