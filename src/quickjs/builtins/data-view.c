f3a3abd1f619aea9f8249157ee5e3793be3e20b7
Yan-Jie Wang <yanjiewtw@gmail.com>
2026-09-25 05:48:08 +0800

    Extract DataView builtin from TypedArray

---
+++ src/quickjs/builtins/data-view.c
@@ -0,0 +1,419 @@
+/*
+ * QuickJS DataView builtin
+ *
+ * Copyright (c) 2017-2025 Fabrice Bellard
+ * Copyright (c) 2017-2025 Charlie Gordon
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "../internal/base.h"
+#include "../internal/runtime.h"
+#include "../internal/number.h"
+#include "../internal/object.h"
+#include "../internal/bytecode.h"
+#include "typed-array.h"
+#include "data-view.h"
+
+JSValue js_dataview_constructor(JSContext *ctx,
+                                       JSValueConst new_target,
+                                       int argc, JSValueConst *argv)
+{
+    BOOL recompute_len = FALSE;
+    BOOL track_rab = FALSE;
+    JSArrayBuffer *abuf;
+    uint64_t offset;
+    uint32_t len;
+    JSValueConst buffer;
+    JSValue obj;
+    JSTypedArray *ta;
+    JSObject *p;
+
+    buffer = argv[0];
+    abuf = js_get_array_buffer(ctx, buffer);
+    if (!abuf)
+        return JS_EXCEPTION;
+    offset = 0;
+    if (argc > 1) {
+        if (JS_ToIndex(ctx, &offset, argv[1]))
+            return JS_EXCEPTION;
+    }
+    if (abuf->detached)
+        return JS_ThrowTypeErrorDetachedArrayBuffer(ctx);
+    if (offset > abuf->byte_length)
+        return JS_ThrowRangeError(ctx, "invalid byteOffset");
+    len = abuf->byte_length - offset;
+    if (argc > 2 && !JS_IsUndefined(argv[2])) {
+        uint64_t l;
+        if (JS_ToIndex(ctx, &l, argv[2]))
+            return JS_EXCEPTION;
+        if (l > len)
+            return JS_ThrowRangeError(ctx, "invalid byteLength");
+        len = l;
+    } else {
+        recompute_len = TRUE;
+        track_rab = array_buffer_is_resizable(abuf);
+    }
+
+    obj = js_create_from_ctor(ctx, new_target, JS_CLASS_DATAVIEW);
+    if (JS_IsException(obj))
+        return JS_EXCEPTION;
+    if (abuf->detached) {
+        /* could have been detached in js_create_from_ctor() */
+        JS_ThrowTypeErrorDetachedArrayBuffer(ctx);
+        goto fail;
+    }
+    // RAB could have been resized in js_create_from_ctor()
+    if (offset > abuf->byte_length) {
+        goto out_of_bound;
+    } else if (recompute_len) {
+        len = abuf->byte_length - offset;
+    } else if (offset + len > abuf->byte_length) {
+    out_of_bound:
+        JS_ThrowRangeError(ctx, "invalid byteOffset or byteLength");
+        goto fail;
+    }
+    ta = js_malloc(ctx, sizeof(*ta));
+    if (!ta) {
+    fail:
+        JS_FreeValue(ctx, obj);
+        return JS_EXCEPTION;
+    }
+    p = JS_VALUE_GET_OBJ(obj);
+    ta->obj = p;
+    ta->buffer = JS_VALUE_GET_OBJ(JS_DupValue(ctx, buffer));
+    ta->offset = offset;
+    ta->length = len;
+    ta->track_rab = track_rab;
+    list_add_tail(&ta->link, &abuf->array_list);
+    p->u.typed_array = ta;
+    return obj;
+}
+
+// is the DataView out of bounds relative to its parent arraybuffer?
+static BOOL dataview_is_oob(JSObject *p)
+{
+    JSArrayBuffer *abuf;
+    JSTypedArray *ta;
+
+    assert(p->class_id == JS_CLASS_DATAVIEW);
+    ta = p->u.typed_array;
+    abuf = ta->buffer->u.array_buffer;
+    if (abuf->detached)
+        return TRUE;
+    if (ta->offset > abuf->byte_length)
+        return TRUE;
+    if (ta->track_rab)
+        return FALSE;
+    return (int64_t)ta->offset + ta->length > abuf->byte_length;
+}
+
+static JSObject *get_dataview(JSContext *ctx, JSValueConst this_val)
+{
+    JSObject *p;
+    if (JS_VALUE_GET_TAG(this_val) != JS_TAG_OBJECT)
+        goto fail;
+    p = JS_VALUE_GET_OBJ(this_val);
+    if (p->class_id != JS_CLASS_DATAVIEW) {
+    fail:
+        JS_ThrowTypeError(ctx, "not a DataView");
+        return NULL;
+    }
+    return p;
+}
+
+static JSValue js_dataview_get_buffer(JSContext *ctx, JSValueConst this_val)
+{
+    JSObject *p;
+    JSTypedArray *ta;
+    p = get_dataview(ctx, this_val);
+    if (!p)
+        return JS_EXCEPTION;
+    ta = p->u.typed_array;
+    return JS_DupValue(ctx, JS_MKPTR(JS_TAG_OBJECT, ta->buffer));
+}
+
+static JSValue js_dataview_get_byteLength(JSContext *ctx, JSValueConst this_val)
+{
+    JSArrayBuffer *abuf;
+    JSTypedArray *ta;
+    JSObject *p;
+
+    p = get_dataview(ctx, this_val);
+    if (!p)
+        return JS_EXCEPTION;
+    if (dataview_is_oob(p))
+        return JS_ThrowTypeErrorArrayBufferOOB(ctx);
+    ta = p->u.typed_array;
+    if (ta->track_rab) {
+        abuf = ta->buffer->u.array_buffer;
+        return JS_NewUint32(ctx, abuf->byte_length - ta->offset);
+    }
+    return JS_NewUint32(ctx, ta->length);
+}
+
+static JSValue js_dataview_get_byteOffset(JSContext *ctx, JSValueConst this_val)
+{
+    JSTypedArray *ta;
+    JSObject *p;
+
+    p = get_dataview(ctx, this_val);
+    if (!p)
+        return JS_EXCEPTION;
+    if (dataview_is_oob(p))
+        return JS_ThrowTypeErrorArrayBufferOOB(ctx);
+    ta = p->u.typed_array;
+    return JS_NewUint32(ctx, ta->offset);
+}
+
+static JSValue js_dataview_getValue(JSContext *ctx,
+                                    JSValueConst this_obj,
+                                    int argc, JSValueConst *argv, int class_id)
+{
+    JSTypedArray *ta;
+    JSArrayBuffer *abuf;
+    BOOL littleEndian, is_swap;
+    int size;
+    uint8_t *ptr;
+    uint32_t v;
+    uint64_t pos;
+
+    ta = JS_GetOpaque2(ctx, this_obj, JS_CLASS_DATAVIEW);
+    if (!ta)
+        return JS_EXCEPTION;
+    size = 1 << typed_array_size_log2(class_id);
+    if (JS_ToIndex(ctx, &pos, argv[0]))
+        return JS_EXCEPTION;
+    littleEndian = argc > 1 && JS_ToBool(ctx, argv[1]);
+    is_swap = littleEndian ^ !is_be();
+    abuf = ta->buffer->u.array_buffer;
+    if (abuf->detached)
+        return JS_ThrowTypeErrorDetachedArrayBuffer(ctx);
+    // order matters: this check should come before the next one
+    if ((pos + size) > ta->length)
+        return JS_ThrowRangeError(ctx, "out of bound");
+    // test262 expects a TypeError for this and V8, in its infinite wisdom,
+    // throws a "detached array buffer" exception, but IMO that doesn't make
+    // sense because the buffer is not in fact detached, it's still there
+    if ((int64_t)ta->offset + ta->length > abuf->byte_length)
+        return JS_ThrowTypeError(ctx, "out of bound");
+    ptr = abuf->data + ta->offset + pos;
+
+    switch(class_id) {
+    case JS_CLASS_INT8_ARRAY:
+        return JS_NewInt32(ctx, *(int8_t *)ptr);
+    case JS_CLASS_UINT8_ARRAY:
+        return JS_NewInt32(ctx, *(uint8_t *)ptr);
+    case JS_CLASS_INT16_ARRAY:
+        v = get_u16(ptr);
+        if (is_swap)
+            v = bswap16(v);
+        return JS_NewInt32(ctx, (int16_t)v);
+    case JS_CLASS_UINT16_ARRAY:
+        v = get_u16(ptr);
+        if (is_swap)
+            v = bswap16(v);
+        return JS_NewInt32(ctx, v);
+    case JS_CLASS_INT32_ARRAY:
+        v = get_u32(ptr);
+        if (is_swap)
+            v = bswap32(v);
+        return JS_NewInt32(ctx, v);
+    case JS_CLASS_UINT32_ARRAY:
+        v = get_u32(ptr);
+        if (is_swap)
+            v = bswap32(v);
+        return JS_NewUint32(ctx, v);
+    case JS_CLASS_BIG_INT64_ARRAY:
+        {
+            uint64_t v;
+            v = get_u64(ptr);
+            if (is_swap)
+                v = bswap64(v);
+            return JS_NewBigInt64(ctx, v);
+        }
+        break;
+    case JS_CLASS_BIG_UINT64_ARRAY:
+        {
+            uint64_t v;
+            v = get_u64(ptr);
+            if (is_swap)
+                v = bswap64(v);
+            return JS_NewBigUint64(ctx, v);
+        }
+        break;
+    case JS_CLASS_FLOAT16_ARRAY:
+        {
+            uint16_t v;
+            v = get_u16(ptr);
+            if (is_swap)
+                v = bswap16(v);
+            return __JS_NewFloat64(ctx, fromfp16(v));
+        }
+    case JS_CLASS_FLOAT32_ARRAY:
+        {
+            union {
+                float f;
+                uint32_t i;
+            } u;
+            v = get_u32(ptr);
+            if (is_swap)
+                v = bswap32(v);
+            u.i = v;
+            return __JS_NewFloat64(ctx, u.f);
+        }
+    case JS_CLASS_FLOAT64_ARRAY:
+        {
+            union {
+                double f;
+                uint64_t i;
+            } u;
+            u.i = get_u64(ptr);
+            if (is_swap)
+                u.i = bswap64(u.i);
+            return __JS_NewFloat64(ctx, u.f);
+        }
+    default:
+        abort();
+    }
+}
+
+static JSValue js_dataview_setValue(JSContext *ctx,
+                                    JSValueConst this_obj,
+                                    int argc, JSValueConst *argv, int class_id)
+{
+    JSTypedArray *ta;
+    JSArrayBuffer *abuf;
+    BOOL littleEndian, is_swap;
+    int size;
+    uint8_t *ptr;
+    uint64_t v64;
+    uint32_t v;
+    uint64_t pos;
+    JSValueConst val;
+
+    ta = JS_GetOpaque2(ctx, this_obj, JS_CLASS_DATAVIEW);
+    if (!ta)
+        return JS_EXCEPTION;
+    size = 1 << typed_array_size_log2(class_id);
+    if (JS_ToIndex(ctx, &pos, argv[0]))
+        return JS_EXCEPTION;
+    val = argv[1];
+    v = 0; /* avoid warning */
+    v64 = 0; /* avoid warning */
+    if (class_id <= JS_CLASS_UINT32_ARRAY) {
+        if (JS_ToUint32(ctx, &v, val))
+            return JS_EXCEPTION;
+    } else if (class_id <= JS_CLASS_BIG_UINT64_ARRAY) {
+        if (JS_ToBigInt64(ctx, (int64_t *)&v64, val))
+            return JS_EXCEPTION;
+    } else {
+        double d;
+        if (JS_ToFloat64(ctx, &d, val))
+            return JS_EXCEPTION;
+        if (class_id == JS_CLASS_FLOAT16_ARRAY) {
+            v = tofp16(d);
+        } else if (class_id == JS_CLASS_FLOAT32_ARRAY) {
+            union {
+                float f;
+                uint32_t i;
+            } u;
+            u.f = d;
+            v = u.i;
+        } else {
+            JSFloat64Union u;
+            u.d = d;
+            v64 = u.u64;
+        }
+    }
+    littleEndian = argc > 2 && JS_ToBool(ctx, argv[2]);
+    is_swap = littleEndian ^ !is_be();
+    abuf = ta->buffer->u.array_buffer;
+    if (abuf->detached)
+        return JS_ThrowTypeErrorDetachedArrayBuffer(ctx);
+    // order matters: this check should come before the next one
+    if ((pos + size) > ta->length)
+        return JS_ThrowRangeError(ctx, "out of bound");
+    // test262 expects a TypeError for this and V8, in its infinite wisdom,
+    // throws a "detached array buffer" exception, but IMO that doesn't make
+    // sense because the buffer is not in fact detached, it's still there
+    if ((int64_t)ta->offset + ta->length > abuf->byte_length)
+        return JS_ThrowTypeError(ctx, "out of bound");
+    ptr = abuf->data + ta->offset + pos;
+
+    switch(class_id) {
+    case JS_CLASS_INT8_ARRAY:
+    case JS_CLASS_UINT8_ARRAY:
+        *ptr = v;
+        break;
+    case JS_CLASS_INT16_ARRAY:
+    case JS_CLASS_UINT16_ARRAY:
+    case JS_CLASS_FLOAT16_ARRAY:
+        if (is_swap)
+            v = bswap16(v);
+        put_u16(ptr, v);
+        break;
+    case JS_CLASS_INT32_ARRAY:
+    case JS_CLASS_UINT32_ARRAY:
+    case JS_CLASS_FLOAT32_ARRAY:
+        if (is_swap)
+            v = bswap32(v);
+        put_u32(ptr, v);
+        break;
+    case JS_CLASS_BIG_INT64_ARRAY:
+    case JS_CLASS_BIG_UINT64_ARRAY:
+    case JS_CLASS_FLOAT64_ARRAY:
+        if (is_swap)
+            v64 = bswap64(v64);
+        put_u64(ptr, v64);
+        break;
+    default:
+        abort();
+    }
+    return JS_UNDEFINED;
+}
+
+const JSCFunctionListEntry js_dataview_proto_funcs[] = {
+    JS_CGETSET_DEF("buffer", js_dataview_get_buffer, NULL ),
+    JS_CGETSET_DEF("byteLength", js_dataview_get_byteLength, NULL ),
+    JS_CGETSET_DEF("byteOffset", js_dataview_get_byteOffset, NULL ),
+    JS_CFUNC_MAGIC_DEF("getInt8", 1, js_dataview_getValue, JS_CLASS_INT8_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getUint8", 1, js_dataview_getValue, JS_CLASS_UINT8_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getInt16", 1, js_dataview_getValue, JS_CLASS_INT16_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getUint16", 1, js_dataview_getValue, JS_CLASS_UINT16_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getInt32", 1, js_dataview_getValue, JS_CLASS_INT32_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getUint32", 1, js_dataview_getValue, JS_CLASS_UINT32_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getBigInt64", 1, js_dataview_getValue, JS_CLASS_BIG_INT64_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getBigUint64", 1, js_dataview_getValue, JS_CLASS_BIG_UINT64_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getFloat16", 1, js_dataview_getValue, JS_CLASS_FLOAT16_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getFloat32", 1, js_dataview_getValue, JS_CLASS_FLOAT32_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("getFloat64", 1, js_dataview_getValue, JS_CLASS_FLOAT64_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setInt8", 2, js_dataview_setValue, JS_CLASS_INT8_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setUint8", 2, js_dataview_setValue, JS_CLASS_UINT8_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setInt16", 2, js_dataview_setValue, JS_CLASS_INT16_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setUint16", 2, js_dataview_setValue, JS_CLASS_UINT16_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setInt32", 2, js_dataview_setValue, JS_CLASS_INT32_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setUint32", 2, js_dataview_setValue, JS_CLASS_UINT32_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setBigInt64", 2, js_dataview_setValue, JS_CLASS_BIG_INT64_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setBigUint64", 2, js_dataview_setValue, JS_CLASS_BIG_UINT64_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setFloat16", 2, js_dataview_setValue, JS_CLASS_FLOAT16_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setFloat32", 2, js_dataview_setValue, JS_CLASS_FLOAT32_ARRAY ),
+    JS_CFUNC_MAGIC_DEF("setFloat64", 2, js_dataview_setValue, JS_CLASS_FLOAT64_ARRAY ),
+    JS_PROP_STRING_DEF("[Symbol.toStringTag]", "DataView", JS_PROP_CONFIGURABLE ),
+};
+
